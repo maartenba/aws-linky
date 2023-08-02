c5382e460aca1229718e12ffbee999198cbbdb26
Maarten Balliauw <maarten@maartenballiauw.be>
2023-08-02 10:26:03 +0200

    Hardcoded set of links, no user-agent check

--- Linky/Program.cs
+++ Linky/Program.cs
@@ -1,6 +1,40 @@
+using Microsoft.Extensions.Primitives;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+var links = new Dictionary<string, DynamicLink>(StringComparer.OrdinalIgnoreCase);
+links.Add("aws", new DynamicLink
+{
+    ShortIdentifier = "aws",
+    WebLink = "https://aws.amazon.com/console/",
+    AndroidMobileLink = "https://play.google.com/store/apps/details?id=com.amazon.aws.console.mobile",
+    AppleMobileLink = "https://apps.apple.com/us/app/aws-console/id580990573"
+});
+
+app.MapGet("/{shortIdentifier}", (string shortIdentifier, HttpContext http) =>
+{
+    if (links.TryGetValue(shortIdentifier, out var dynamicLink))
+    {
+        http.Response.StatusCode = 301;
+        http.Response.Headers.Location = new StringValues(dynamicLink.WebLink);
+        http.Response.WriteAsync("Redirecting...");
+        return;
+    }
+
+    http.Response.StatusCode = 404;
+    http.Response.WriteAsync("Page not found");
+});
+
 app.Run();
+
+public class DynamicLink
+{
+    public int Id { get; set; }
+    public string ShortIdentifier { get; set; }
+    public string WebLink { get; set; }
+    public string? AndroidMobileLink { get; set; }
+    public string? AppleMobileLink { get; set; }
+}
